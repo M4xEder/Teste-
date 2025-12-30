06ed9bfdb89200ca794c099f5f02a29b14242edb
Max_Elder <maxez1999@gmail.com>
2025-12-29 21:17:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 
+// Scroll vertical controlando horizontal
+window.addEventListener("scroll", () => {
+  const scrollTop = window.scrollY;
+  document.querySelector(".horizontal").style.transform =
+    `translateX(-${scrollTop}px)`;
+});
+
+document.body.style.height = "400vw";
+
+// Intersection Observer
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+document.querySelectorAll(".fade, .slide-left").forEach(el => {
+  observer.observe(el);
+});
